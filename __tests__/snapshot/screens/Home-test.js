74bb8e6fc704ce3162072bf36eabfe056cfdc3c9
hjp <harry.pugh@acuris.com>
2020-08-09 21:19:05 +0100

    Fix tests

--- __tests__/snapshot/screens/Home-test.js
+++ __tests__/snapshot/screens/Home-test.js
@@ -2,7 +2,7 @@ import 'react-native'
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import HomeScreen from '../../../src/Components/Screens/Settings'
+import HomeScreen from '../../../src/Components/Screens/Home'
 
 it('renders correctly', () => {
   const homeScreen = renderer.create(<HomeScreen />)
