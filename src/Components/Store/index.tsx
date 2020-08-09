345608c9891b8218a27535eeebf450a484bfe853
hjp <harry.pugh@acuris.com>
2020-08-09 11:28:27 +0100

    Update readings only every hour

--- src/Components/Store/index.tsx
+++ src/Components/Store/index.tsx
@@ -5,7 +5,7 @@ export const storeData = async (key: string, data: any): Promise<any> => {
     const value = JSON.stringify(data)
     await AsyncStorage.setItem(key, value)
   } catch(err) {
-    console.log('Error: ', err)
+    console.log('Error storeData: ', err)
   }
 }
 
@@ -14,6 +14,22 @@ export const getData = async (key: string): Promise<any> => {
     const value = await AsyncStorage.getItem(key)
     return value && JSON.parse(value)
   } catch(err) {
-    console.log('Error: ', err)
+    console.log('Error getData: ', err)
   }
+}
+
+export const needsUpdating = async (key: string): Promise<boolean> => {
+  try {
+    const data = await getData(key)
+    const lastUpdated = data && data.updated
+
+    if (data === null || (Date.now() - lastUpdated) > 3600000) {
+      console.log(`${key} updated`)
+      return true
+    }
+  } catch(err) {
+    console.log('Error needsUpdating: ', err)
+  }
+
+  return false
 }
