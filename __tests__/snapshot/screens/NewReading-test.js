e5e950b31269e5d9de2cd7dd6bc279ce817e8bb1
hjp <harry.pugh@acuris.com>
2020-08-09 21:19:05 +0100

    Fix tests

--- __tests__/snapshot/screens/NewReading-test.js
+++ __tests__/snapshot/screens/NewReading-test.js
@@ -2,7 +2,7 @@ import 'react-native'
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import NewReadingScreen from '../../../src/Components/Screens/Settings'
+import NewReadingScreen from '../../../src/Components/Screens/NewReading'
 
 it('renders correctly', () => {
   const newReadingScreen = renderer.create(<NewReadingScreen />)
