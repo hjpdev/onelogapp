b06a5775cddc0f261c153c92fbb607d2086440bd
hjp <harry.pugh@acuris.com>
2020-08-09 13:06:56 +0100

    Fix more typings

--- src/Components/Carousel/Readings/Macro.tsx
+++ src/Components/Carousel/Readings/Macro.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-interface MacroReadingProps {
-  data: {[key: string]: number};
+import { MacroTemplateProps } from '../index'
+
+export interface IMacroReading {
+  kcal: number,
+  carbs: number,
+  sugar: number,
+  protein: number,
+  fat: number
 }
 
-const MacroReading: React.FC<MacroReadingProps> = (props: MacroReadingProps) => {
+const MacroReading: React.FC<MacroTemplateProps> = (props: MacroTemplateProps) => {
   const { data } = props
     const { kcal, carbs, sugar, protein, fat } = data
 
