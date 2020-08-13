a6af9416121d6a839194a852f91bc930047c067d
hjp <harry.pugh@acuris.com>
2020-08-13 10:53:42 +0100

    Add tests for NavBar

--- __tests__/unit/testNavBar.js
+++ __tests__/unit/testNavBar.js
@@ -1,11 +1,45 @@
 import 'react-native'
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { render, fireEvent } from '@testing-library/react-native'
 
 import NavBar from '../../src/Components/NavBar'
 
-it('renders correctly', () => {
-  const navBar = renderer.create(<NavBar />)
+it('renders five nav links', () => {
+  const { getByTestId } = render(<NavBar />)
 
-  expect(navBar).toMatchSnapshot()
+  expect(getByTestId('nav-link-settings')).toBeTruthy()
+  expect(getByTestId('nav-link-account')).toBeTruthy()
+  expect(getByTestId('nav-link-home')).toBeTruthy()
+  expect(getByTestId('nav-link-new-reading')).toBeTruthy()
+  expect(getByTestId('nav-link-analytics')).toBeTruthy()
+})
+
+it('changes the current screen when nav link is pressed', () => {
+  const mockSetCurrentScreen = jest.fn()
+  const { getByTestId } = render(<NavBar setCurrentScreen={mockSetCurrentScreen} />)
+
+  fireEvent.press(getByTestId('nav-link-settings'))
+
+  expect(mockSetCurrentScreen.mock.calls.length).toBe(1)
+  expect(mockSetCurrentScreen.mock.calls[0][0]).toBe('settings')
+
+  fireEvent.press(getByTestId('nav-link-account'))
+
+  expect(mockSetCurrentScreen.mock.calls.length).toBe(2)
+  expect(mockSetCurrentScreen.mock.calls[1][0]).toBe('account')
+
+  fireEvent.press(getByTestId('nav-link-home'))
+
+  expect(mockSetCurrentScreen.mock.calls.length).toBe(3)
+  expect(mockSetCurrentScreen.mock.calls[2][0]).toBe('home')
+
+  fireEvent.press(getByTestId('nav-link-new-reading'))
+
+  expect(mockSetCurrentScreen.mock.calls.length).toBe(4)
+  expect(mockSetCurrentScreen.mock.calls[3][0]).toBe('newReading')
+
+  fireEvent.press(getByTestId('nav-link-analytics'))
+
+  expect(mockSetCurrentScreen.mock.calls.length).toBe(5)
+  expect(mockSetCurrentScreen.mock.calls[4][0]).toBe('analytics')
 })
