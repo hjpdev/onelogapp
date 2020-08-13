62467f22de3f8bba6eeae9a1bc3aa29f77f5448f
hjp <harry.pugh@acuris.com>
2020-08-13 10:24:05 +0100

    Add test for Main Header

--- __tests__/unit/testMainHeader.js
+++ __tests__/unit/testMainHeader.js
@@ -1,11 +1,12 @@
 import 'react-native'
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { render } from '@testing-library/react-native'
 
 import MainHeader from '../../src/Components/MainHeader'
 
-it('renders correctly', () => {
-  const mainHeader = renderer.create(<MainHeader />)
+it('renders with the correct title', () => {
+  const { getByTestId, getByText } = render(<MainHeader />)
 
-  expect(mainHeader).toMatchSnapshot()
+  expect(getByTestId('main-header')).toBeTruthy()
+  expect(getByText('Onelog')).toBeTruthy()
 })
