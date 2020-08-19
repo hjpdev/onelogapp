cd4d9c6e54060eaa4986c38456b50265d65cb7c8
hjp <harry.pugh@acuris.com>
2020-08-19 12:43:10 +0100

    Basic layout

--- src/Components/Screens/NewReading.tsx
+++ src/Components/Screens/NewReading.tsx
@@ -1,81 +1,55 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
+import GradientBorder from '../Minor/GradientBorder'
+
 import { ScreenStyles } from '../../Assets/Styles/Screen'
 
 const NewReadingScreen: React.FC = () => {
   return(
     <View style={ScreenStyles.container} testID={'new-reading-screen'}>
-        <View style={Styles.row}>
-          <View style={{ ...Styles.topPill, alignItems: 'flex-end' }}>
-            <TouchableOpacity>
-              <Text style={Styles.text}>{'Macro'}</Text>
-            </TouchableOpacity>
-          </View>
+      <View style={Styles.newReadings}>
+        <GradientBorder x={1.0} y={1.0} />
+        <TouchableOpacity style={Styles.newReading}>
+          <Text style={Styles.newReadingText}>{'Bg'}</Text>
+        </TouchableOpacity>
+        <GradientBorder x={1.0} y={1.0} />
 
-          <View style={{ ...Styles.topPill, alignItems: 'flex-start' }}>
-            <TouchableOpacity>
-              <Text style={Styles.text}>{'Bg'}</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <TouchableOpacity style={Styles.newReading}>
+          <Text style={Styles.newReadingText}>{'Dose'}</Text>
+        </TouchableOpacity>
+        <GradientBorder x={1.0} y={1.0} />
 
-        <View style={Styles.row}>
-          <View style={{ ...Styles.bottomPill, alignItems: 'flex-end' }}>
-            <TouchableOpacity>
-              <Text style={Styles.text}>{'Keto'}</Text>
-            </TouchableOpacity>
-          </View>
+        <TouchableOpacity style={Styles.newReading}>
+          <Text style={Styles.newReadingText}>{'Macro'}</Text>
+        </TouchableOpacity>
+        <GradientBorder x={1.0} y={1.0} />
 
-          <View style={{ ...Styles.bottomPill, alignItems: 'flex-start' }}>
-            <TouchableOpacity>
-              <Text style={Styles.text}>{'Dose'}</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <TouchableOpacity style={Styles.newReading}>
+          <Text style={Styles.newReadingText}>{'Ketones'}</Text>
+        </TouchableOpacity>
+        <GradientBorder x={1.0} y={1.0} />
+      </View>
     </View>
   )
 }
 
 const Styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row',
-    flex: 1,
-    height: '100%',
+  newReadings: {
+    flex: 9,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'blue',
+    width: '100%'
+  },
+  newReading: {
+    alignItems: 'center',
+    // backgroundColor: 'yellow',
     width: '100%',
-    backgroundColor: '#ebebeb'
+    padding: 40
   },
-  topPill: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    color: 'white',
-    backgroundColor: '#ebebeb',
-    margin: 20
-  },
-  bottomPill: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    color: 'white',
-    margin: 20
-  },
-  text: {
-    fontSize: 18,
-    fontFamily: 'roboto',
-    fontWeight: 'bold',
-    backgroundColor: '#ebebeb',
-    padding: 50,
-    color: '#3f3d3d',
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: '#1a1919',
-    borderBottomWidth: 8,
-    borderBottomColor: '#252424',
-    width: 160,
-    height: 120,
-    textAlign: 'center',
-    textAlignVertical: 'center'
+  newReadingText: {
+    fontSize: 22
   }
 })
 
