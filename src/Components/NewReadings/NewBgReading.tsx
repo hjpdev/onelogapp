d5c4551a0ce720e17e0a66852976cf095ad81c7a
hjp <harry.pugh@acuris.com>
2020-08-19 15:30:30 +0100

    Add shell components & logic for selecting new reading type

---
+++ src/Components/NewReadings/NewBgReading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const NewBgReading: React.FC = () => {
+  return(
+    <View>
+      <Text>{'BG'}</Text>
+    </View>
+  )
+}
