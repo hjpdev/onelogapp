fd26d311510e7596755cd6dd9ab21e4caceaa9d0
hjp <harry.pugh@acuris.com>
2020-08-19 15:34:52 +0100

    Rename NewReading folder and remove semi-colons

--- src/Components/Screens/NewReading.tsx
+++ src/Components/Screens/NewReading.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import { BackHandler, View } from 'react-native'
 
-import { NewReadingSelection, NewBgReading, NewDoseReading, NewMacroReading, NewKetoReading } from '../NewReadings'
+import { NewReadingSelection, NewBgReading, NewDoseReading, NewMacroReading, NewKetoReading } from '../NewReading'
 
 import { ScreenStyles } from '../../Assets/Styles/Screen'
 
@@ -19,15 +19,15 @@ const NewReadingScreen: React.FC = () => {
     const backAction = () => {
       setNewReadingType('')
       return true
-    };
+    }
 
     const backHandler = BackHandler.addEventListener(
       "hardwareBackPress",
       backAction
-    );
+    )
 
-    return () => backHandler.remove();
-  }, []);
+    return () => backHandler.remove()
+  }, [])
 
   return(
     <View style={ScreenStyles.container} testID={'new-reading-screen'}>
