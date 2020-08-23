1bbd38b3015f8e4cd4752992c8a05917d7cd4d4d
hjp <harry.pugh@acuris.com>
2020-08-23 17:50:47 +0100

    WIP - Add ability to submit new bg reading

--- src/Components/NewReading/NewBgReading.tsx
+++ src/Components/NewReading/NewBgReading.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 
 import WheelSelector from '../Minor/WheelSelector'
 import TimeSelector from '../Minor/TimeSelector'
@@ -8,7 +8,26 @@ export const NewBgReading: React.FC = () => {
   const [reading, setReading] = useState(0.0)
   const [dateTime, setDateTime] = useState(null)
 
-  console.log('BG DATE TIME => ', dateTime)
+  const submitReading = async () => {
+    const url = 'http://localhost:8088/readings/bg'
+    const data = dateTime ? { reading, created: dateTime } : { reading }
+
+    try {
+      if (reading >= 0) {
+        const result = await fetch(url, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        return result && result.json()
+      }
+    } catch (err) {
+      console.log('Error submitReading: ', err)
+    }
+  }
 
   return(
     <View>
@@ -16,6 +35,9 @@ export const NewBgReading: React.FC = () => {
       <Text>{reading}</Text>
       <TimeSelector setDateTime={setDateTime} />
       <WheelSelector updateReading={setReading} />
+      <TouchableOpacity onPress={async() => await submitReading()}>
+        <Text>{'Submit'}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
