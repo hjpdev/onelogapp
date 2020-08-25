b65aede3112e739accfed2c90304643b02fa99e2
hjp <harry.pugh@acuris.com>
2020-08-25 21:55:38 +0100

    Add ability to submit new macro reading

--- src/Components/NewReading/Macro.tsx
+++ src/Components/NewReading/Macro.tsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import MacroReadingInput from '../Minor/MacroReadingInput'
 import TimeSelector from '../Minor/TimeSelector'
+import { submitReading } from '../../Helpers/Data'
 
 export const NewMacroReading: React.FC = () => {
   const [reading, setReading] = useState({})
   const [dateTime, setDateTime] = useState(null)
+
+  const handleSubmit = async () => {
+    if (Object.keys(reading).length > 0) {
+      if (!Object.keys(reading).every(macro => { return reading[macro] === 0 })) {
+        const data = dateTime ? { ...reading, created: dateTime } : { ...reading }
+        await submitReading('macro', data)
+      }
+    }
+  }
 
   return(
     <View style={Styles.container}>
       <Text>{'Macro'}</Text>
       <TimeSelector setDateTime={setDateTime} />
       <MacroReadingInput updateReading={setReading} />
+      <TouchableOpacity onPress={async() => await handleSubmit()} style={Styles.submit}>
+        <Text style={Styles.submitText}>{'Submit'}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -26,4 +39,12 @@ const Styles = StyleSheet.create({
     alignItems: 'center',
     height: '100%'
   },
+  submit: {
+    padding: 20,
+    backgroundColor: '#c4c4c4'
+  },
+  submitText: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 })
