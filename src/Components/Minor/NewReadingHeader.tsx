cd453a38ddeebdf333e51b6606b0b39319bafb12
hjp <harry.pugh@acuris.com>
2020-08-26 14:17:16 +0100

    Update style for new reading header

--- src/Components/Minor/NewReadingHeader.tsx
+++ src/Components/Minor/NewReadingHeader.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Image, View, StyleSheet, Text, TouchableOpacity } from 'react-native'
 
+import GradientBorder from './GradientBorder'
+
 type NewReadingHeaderProps = {
   text: string,
   onBack: () => void
@@ -10,11 +12,14 @@ const NewReadingHeader: React.FC<NewReadingHeaderProps> = (props: NewReadingHead
   const { text, onBack } = props
 
   return(
+    <>
     <View style={Styles.header}>
       <TouchableOpacity onPress={onBack}><Image source={require('../../Assets/Images/BackArrow.png')} style={{height: 30, width: 30}} /></TouchableOpacity>
       <Text style={Styles.text}>{text}</Text>
       <TouchableOpacity><Image source={require('../../Assets/Images/PreviousReadings.png')} style={{height: 40, width: 30}} /></TouchableOpacity>
     </View>
+    <GradientBorder x={1.0} y={1.0} />
+    </>
   )
 }
 
@@ -27,7 +32,8 @@ const Styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'space-around',
     alignItems: 'center',
-    paddingTop: 8
+    paddingTop: 8,
+    paddingBottom: 8
   },
   text: {
     fontSize: 20
