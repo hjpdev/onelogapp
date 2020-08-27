0f9fbc1de5fa802ca375026f4f765e0602f939bc
hjp <harry.pugh@acuris.com>
2020-08-27 17:44:46 +0100

    WIP - Implement general idea for displaying previous readings

--- src/Components/PreviousReadings/index.tsx
+++ src/Components/PreviousReadings/index.tsx
@@ -1,34 +1,38 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
+import PreviousReadingsForDate from './PreviousReadingsForDate'
 import NewReadingHeader from '../Minor/NewReadingHeader'
 
-type PreviousReadingsModalProps = {
+type PreviousReadingsProps = {
   onBack: () => void
 }
 
-const PreviousReadingsModal: React.FC<PreviousReadingsModalProps> = (props: PreviousReadingsModalProps) => {
+const PreviousReadings: React.FC<PreviousReadingsProps> = (props: PreviousReadingsProps) => {
   const { onBack } = props
+
+  const generateListItems = () => {
+    const dates = ['26/08', '25/08', '24/08']
+    return dates.map(date => {
+      return <PreviousReadingsForDate date={date} key={date} />
+    })
+  }
 
   return(
     <>
     <NewReadingHeader text={'Previous Bg Readings'} hidePreviousIcon onBack={onBack} onShowPrevious={() => null} />
     <View style={Styles.container}>
-      <View>
-        <Text>{'PreviousReadings'}</Text>
-      </View>
+      {generateListItems()}
     </View>
     </>
   )
 }
 
-export default PreviousReadingsModal
+export default PreviousReadings
 
 
 const Styles = StyleSheet.create({
   container: {
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
     height: '92%'
   }
 })
