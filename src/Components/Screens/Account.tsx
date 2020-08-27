0b0dac049b24fdbfa013d003f11b723428ad1dd5
hjp <harry.pugh@acuris.com>
2020-08-27 23:00:40 +0100

    Add NavBar to all screens

--- src/Components/Screens/Account.tsx
+++ src/Components/Screens/Account.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import NavBar from '../NavBar'
 import { ScreenStyles } from '../../Assets/Styles/Screen'
 
 const AccountScreen: React.FC = () => {
   return(
+    <>
     <View style={ScreenStyles.container} testID={'account-screen'}>
       <Text style={ScreenStyles.text}>
         {'Account'}
       </Text>
     </View>
+    <NavBar />
+    </>
   )
 }
 
--- src/Components/Screens/Analytics.tsx
+++ src/Components/Screens/Analytics.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import NavBar from '../NavBar'
 import { ScreenStyles } from '../../Assets/Styles/Screen'
 
 const AnalyticsScreen: React.FC = () => {
   return(
+    <>
     <View style={ScreenStyles.container} testID={'analytics-screen'}>
       <Text style={ScreenStyles.text}>
         {'Analytics'}
       </Text>
     </View>
+    <NavBar />
+    </>
   )
 }
 
